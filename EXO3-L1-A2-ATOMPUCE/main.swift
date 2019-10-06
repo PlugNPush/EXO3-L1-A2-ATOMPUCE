2e1a13dfd5de2342641dbebbe9eb0ec0bb9a8770
PlugNCS <michael.nass@free.fr>
2019-10-06 23:09:52 +0200

    Initial Commit

---
+++ EXO3-L1-A2-ATOMPUCE/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EXO3-L1-A2-ATOMPUCE
+//
+//  Created by PlugN on 06/10/2019.
+//  Copyright © 2019 Groupe MINASTE. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
